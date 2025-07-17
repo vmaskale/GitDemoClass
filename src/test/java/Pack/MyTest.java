e4be624f014712e100c8a9cde3fbe81a3f9c66d2
vaishnavi <vmaskale@gmail.com>
2025-07-17 12:11:57 +0800

    commiting demo code

---
+++ src/test/java/Pack/MyTest.java
@@ -0,0 +1,17 @@
+package Pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyTest {
+
+	@Test
+	public void methodone() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver Driver = new ChromeDriver();
+	}
+	
+	
+	
+}
